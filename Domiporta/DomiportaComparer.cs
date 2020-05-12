aabf551d3c984f1484025d6e34d3e1f5d8d049be
PGdk <s170624@student.pg.edu.pl>
2020-05-13 01:24:02 +0200

    projekt v.2

---
+++ Domiporta/DomiportaComparer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Domiporta
+{
+    public class DomiportaComparer : IEqualityComparer<Entry>
+    {
+        public bool Equals([AllowNull] Entry x, [AllowNull] Entry y)
+        {
+            if (GetHashCode(x) == GetHashCode(y))
+                return true;
+
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] Entry obj)
+        {
+            //return obj.OfferDetails.Url == null ? 0 : obj.OfferDetails.Url.GetHashCode();
+            return obj.OfferDetails.Url == null ? 0 : obj.OfferDetails.Url.GetHashCode() +
+                   obj.PropertyPrice.TotalGrossPrice.GetHashCode() +
+                   obj.PropertyPrice.PricePerMeter.GetHashCode() +
+                   obj.PropertyAddress.City.GetHashCode() +
+                   obj.PropertyAddress.StreetName.GetHashCode() +
+                   obj.PropertyDetails.Area.GetHashCode() +
+                   obj.PropertyDetails.NumberOfRooms.GetHashCode() +
+                   obj.PropertyDetails.YearOfConstruction.GetHashCode();
+        }
+    }
+}
