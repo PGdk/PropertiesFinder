b522844701324f39dba92f197ceb972cf99ed9ce
PGdk <64419068+PGdk@users.noreply.github.com>
2020-05-12 23:40:40 +0200

    Poprawione pliki
    - uwzględnione uwagi prowadzącego

--- DomiportaComparer.cs
+++ DomiportaComparer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Models;
 using System.Diagnostics.CodeAnalysis;
 
@@ -7,7 +6,7 @@ namespace Application
 {
         public class DomiportaComparer : IEqualityComparer<Entry>
         {
-            public bool Equals(Entry x, Entry y)
+            public bool Equals([AllowNull] Entry x, [AllowNull] Entry y)
             {
                 if (GetHashCode(x) == GetHashCode(y))
                     return true;
@@ -21,6 +20,7 @@ namespace Application
             return obj.OfferDetails.Url == null ? 0 : obj.OfferDetails.Url.GetHashCode() +
                    obj.PropertyPrice.TotalGrossPrice.GetHashCode() +
                    obj.PropertyPrice.PricePerMeter.GetHashCode() +
+                   obj.PropertyAddress.City.GetHashCode() +
                    obj.PropertyAddress.StreetName.GetHashCode() +
                    obj.PropertyDetails.Area.GetHashCode() +
                    obj.PropertyDetails.NumberOfRooms.GetHashCode() +
--- DomiportaIntegration.cs
+++ DomiportaIntegration.cs
@@ -2,9 +2,6 @@
 using Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics.Tracing;
 using HtmlAgilityPack;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -33,14 +30,14 @@ namespace Application
                 WebPageFeatures = new WebPageFeatures
                 {
                     HomeSale = true,
-                    HomeRental = false,
+                    HomeRental = true,
                     HouseSale = false,
                     HouseRental = false
                 }
             };
         }
 
-        public Dump GenerateDump() // <--- TODO
+        public Dump GenerateDump()
         {
             //var random = new Random();
             //var randomValue = random.Next() % 10;
@@ -49,23 +46,35 @@ namespace Application
 
             var web = new HtmlAgilityPack.HtmlWeb();
 
-            for (int i = 1; i <= 1; i++) // i = ilosc stron
+            //sale
+            for (int i = 1; i <= 2; i++) // i = ilosc stron
             {
                 var doc = web.Load("https://www.domiporta.pl/mieszkanie/sprzedam?Rodzaj=Bezposrednie&PageNumber=" + i);
+                
+                AddLinksToList(doc, LinksToAds);
+            }
+
+            //rental
+            for (int i = 1; i <= 2; i++)
+            {
+                var doc = web.Load("https://www.domiporta.pl/mieszkanie/wynajme?rodzaj=bezposrednie&pagenumber=" + i);
 
                 AddLinksToList(doc, LinksToAds);
             }
 
-            Console.WriteLine(LinksToAds.Count); // TMP
+            // TMP - licznik stron
+            //Console.WriteLine(LinksToAds.Count);
 
             var EntriesTmp = new List<Entry>();
 
+            // dodawanie Entriskow
+
             for (int i = 0; i < LinksToAds.Count; i++)
             {
-                var doc = web.Load("https://www.domiporta.pl"+LinksToAds[i]);
-                EntriesTmp.Add(CreateEntry(doc, LinksToAds[i])); 
-            }    
- 
+                var doc = web.Load("https://www.domiporta.pl" + LinksToAds[i]);
+                EntriesTmp.Add(CreateEntry(doc, LinksToAds[i]));
+            }
+
             return new Dump
             {
                 DateTime = DateTime.Now,
@@ -74,21 +83,20 @@ namespace Application
             };
 
         }
-
         private static Entry CreateEntry(HtmlDocument htmldoc, string link)
         {
             return new Entry
             {
                 OfferDetails = new OfferDetails
                 {
-                    //Url = $"{WebPage.Url}{link}",
                     Url = $"https://www.domiporta.pl{link}",
                     CreationDateTime = DateTime.Now,
-                    OfferKind = OfferKind.SALE,
+
+                    OfferKind = GetOfferKind(link),
+
                     SellerContact = new SellerContact
                     {
-                        //Email = "okazje@mieszkania.pl"
-                        Telephone = htmldoc.DocumentNode.Descendants("a").Where(d => d.Attributes.Contains("data-tel")).FirstOrDefault().Attributes["data-tel"].Value,
+                        Telephone = GetTelephone(htmldoc),
                     },
                     IsStillValid = true
                 },
@@ -101,29 +109,173 @@ namespace Application
                     PricePerMeter = ParseToDecimalPerM(htmldoc.DocumentNode.Descendants("span").Where(d => d.Attributes.Contains("class") &&
                     d.Attributes["class"].Value.Contains("summary__subtitle summary__subtitle--price")).FirstOrDefault().InnerText),
                 },
-
-                PropertyAddress = new PropertyAddress
-                {
-                    // PolishCity TODO
-                    //City = htmldoc.DocumentNode.Descendants("span").Where(d => d.Attributes.Contains("itemprop") &&
-                    //d.Attributes["itemprop"].Value.Contains("price")).FirstOrDefault().InnerText,
-
-                    //ulica zawiera tez numer - male szanse na wyluskanie osobno
-                    StreetName = htmldoc.DocumentNode.Descendants("span").Where(d => d.Attributes.Contains("itemprop") &&
-                    d.Attributes["itemprop"].Value.Contains("streetAddress")).FirstOrDefault().InnerText,
-
-                },
-
+               
+                PropertyAddress = GetPropertyAddress(htmldoc),
+         
                 PropertyDetails = new PropertyDetails
-                { 
+                {
                     NumberOfRooms = GetIntProperty(htmldoc, "Liczba pokoi"),
                     FloorNumber = GetIntProperty(htmldoc, "Piętro"),
                     YearOfConstruction = GetIntProperty(htmldoc, "Rok budowy"),
                     Area = GetDecProperty(htmldoc, "Powierzchnia całkowita")
                 },
 
-                RawDescription = "",
+                RawDescription = GetRawDescription(htmldoc),
             };
+        }
+
+        private static PropertyAddress GetPropertyAddress(HtmlDocument htmldoc)
+        {
+            string city;
+
+            try
+            {
+                city = htmldoc.DocumentNode.Descendants("span").Where(d => d.Attributes.Contains("itemprop") &&
+                       d.Attributes["itemprop"].Value.Contains("addressLocality")).FirstOrDefault().InnerText.Replace(" ", "_").ToUpper();
+                
+                city = city.Replace("Ą", "A");
+                city = city.Replace("Ć", "C");
+                city = city.Replace("Ę", "E");
+                city = city.Replace("Ł", "L");
+                city = city.Replace("Ó", "O");
+                city = city.Replace("Ś", "S");
+                city = city.Replace("Ź", "Z");
+                city = city.Replace("Ż", "Z");
+            }
+            catch
+            {
+                city = "";
+            }
+
+            try
+            {
+                PolishCity _City = (PolishCity)Enum.Parse(typeof(PolishCity), city);
+
+            return new PropertyAddress
+            {
+                City = _City,
+
+                //District = htmldoc.DocumentNode.SelectSingleNode("//span[@itemprop='addressLocality']").NextSibling.NextSibling.InnerText,
+                District = GetDistrict(htmldoc),
+                StreetName = GetStreetName(htmldoc),
+                DetailedAddress = GetNr(htmldoc),
+            };
+
+            }
+            catch
+            {
+                return new PropertyAddress
+                {
+                    District = GetDistrict(htmldoc),
+                    StreetName = GetStreetName(htmldoc),
+                    DetailedAddress = GetNr(htmldoc),
+                };
+            }
+        }
+
+        private static string GetNr(HtmlDocument htmldoc)
+        {
+            string Nr = "";
+
+            var StreetNode = htmldoc.DocumentNode.SelectSingleNode("//span[@itemprop='streetAddress']");
+
+            if (StreetNode != null)
+            {
+                Nr = StreetNode.InnerText;
+                string[] NrSplit = Nr.Split(' ');
+
+                Nr = ParseNr(NrSplit.Last());
+            }
+
+            return Nr;
+        }
+
+        private static string GetDistrict(HtmlDocument htmldoc)
+        {
+            var DistrictNode = htmldoc.DocumentNode.SelectSingleNode("//span[@itemprop='addressLocality']");
+
+            HtmlAttribute att = DistrictNode.NextSibling.NextSibling.Attributes["itemprop"];
+
+            if (att == null && DistrictNode != null)
+                return DistrictNode.NextSibling.NextSibling.InnerText;
+
+            else
+                return "";
+        }
+
+        private static string GetStreetName(HtmlDocument htmldoc)
+        {
+            string Street = "";
+            string OnlyStreet = "";
+            //var StreetNode = htmldoc.DocumentNode.Descendants("span").Where(d => d.Attributes.Contains("itemprop") &&
+            //              d.Attributes["itemprop"].Value.Contains("streetAddress"));
+
+            var StreetNode = htmldoc.DocumentNode.SelectSingleNode("//span[@itemprop='streetAddress']");
+
+            if (StreetNode != null)
+            {
+                Street = StreetNode.InnerText;
+               
+                string[] StreetSplit = Street.Split(' ');
+
+                if (StreetSplit.Length == 1)
+                    OnlyStreet = StreetSplit[0];
+
+                if (ParseNr(StreetSplit.Last()) != "") // czyli jest numer
+                {
+                    for (int i = 0; i < (StreetSplit.Length-1); i++)
+                        OnlyStreet += $"{StreetSplit[i] }";
+                }
+            
+            }
+            
+            return OnlyStreet;
+        }
+
+        private static string GetRawDescription(HtmlDocument htmldoc)
+        {
+            var nodes = htmldoc.DocumentNode.SelectNodes("//dt[@class='features__item_name']");
+
+            string value = "";
+
+            foreach (var node in nodes)
+            {
+                if (node.InnerHtml.Contains("Informacje dodatkowe"))
+                {
+                    try
+                    {
+                        value = node.NextSibling.InnerHtml;
+                    }
+                    catch
+                    {
+                        value = "";
+                    }
+                }
+            }
+            return value;
+        }
+
+        private static string GetTelephone(HtmlDocument htmldoc)
+        {
+            string tel;
+
+            try
+            {
+                tel = htmldoc.DocumentNode.Descendants("a").Where(d => d.Attributes.Contains("data-tel")).FirstOrDefault().Attributes["data-tel"].Value;
+            }
+            catch
+            {
+                tel = "brak";
+            }
+            return tel;
+        }
+
+        private static OfferKind GetOfferKind(string link)
+        {   
+            if (link.Contains("wynajme"))
+                return OfferKind.RENTAL;
+            else
+                return OfferKind.SALE;
         }
 
         private static int GetIntProperty(HtmlDocument htmldoc, string value)
@@ -170,6 +322,14 @@ namespace Application
                 }
             }
             return ret;
+        }
+
+        private static string ParseNr(string value)
+        {
+            Regex re = new Regex(@"^[0-9]");
+            Match m = re.Match(value);
+
+            return m.Value;
         }
 
         private static decimal ParseToDecimal(string value)
@@ -221,14 +381,12 @@ namespace Application
             foreach (HtmlNode link in htmldoc.DocumentNode.SelectNodes("//a[@class='sneakpeak__button_a']"))
             {
                 HtmlAttribute att = link.Attributes["href"];
-                //HtmlAttribute att2 = link.Attributes["class"];
 
                 if (att.Value != null)
                 {
                     LinksToAds.Add(att.Value);
                 }
             }
-
         }
 
     }
