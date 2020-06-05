5230b16981dd6b42b4d556e9749af4c7f5d02a7c
Kamil Werner <kamilwerner50@gmail.com>
2020-06-05 23:04:43 +0200

    IntegrationApi - EntriesController.cs - added entries controller

---
+++ IntegrationApi/Controllers/EntriesController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatabaseConnection;
+using Models;
+
+namespace IntegrationApi.Controllers
+{
+    [ApiController]
+    public class EntriesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public EntriesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /entries
+        [Route("entries")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Entry>>> GetEntries()
+        {
+            return await _context.Entries
+                .Include(e => e.OfferDetails)
+                .ThenInclude(of => of.SellerContact)
+                .Include(e => e.PropertyPrice)
+                .Include(e => e.PropertyDetails)
+                .Include(e => e.PropertyAddress)
+                .Include(e => e.PropertyFeatures)
+                .ToListAsync();
+        }
+
+        // GET: /entry/5
+        [Route("entry/{id}")]
+        [HttpGet]
+        public async Task<ActionResult<Entry>> GetEntry(int id)
+        {
+            var entry = await _context.Entries
+                .Include(e => e.OfferDetails)
+                .ThenInclude(of => of.SellerContact)
+                .Include(e => e.PropertyPrice)
+                .Include(e => e.PropertyDetails)
+                .Include(e => e.PropertyAddress)
+                .Include(e => e.PropertyFeatures)
+                .SingleOrDefaultAsync(e => id == e.Id);
+
+            if (null == entry)
+            {
+                return NotFound();
+            }
+
+            return entry;
+        }
+    }
+}
