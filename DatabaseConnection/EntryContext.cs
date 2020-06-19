f1e0041a94ed045f0b7f69722162574e685a2579
Filip Biedrzycki <filipbiedrzycki@gmail.com>
2020-06-19 02:33:48 +0200

    add phase 2 poc

---
+++ DatabaseConnection/EntryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DatabaseConnection
+{
+    public class EntryContext : DbContext
+    {
+        public EntryContext(DbContextOptions<EntryContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Entry> Entries { get; set; }
+    }
+}
