bdb404e409cb2b168b1f510c31644074096508e3
Filip Biedrzycki <filipbiedrzycki@gmail.com>
2020-06-19 15:45:02 +0200

    add Entries EF & CRUD API

--- DatabaseConnection/EntryContext.cs
+++ DatabaseConnection/EntryContext.cs
@@ -5,10 +5,12 @@ namespace DatabaseConnection
 {
     public class EntryContext : DbContext
     {
+        public const string ConnectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=FilipB137248;Integrated Security=True";
         public EntryContext(DbContextOptions<EntryContext> options) : base(options)
         {
-
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer(ConnectionString);
+
         public DbSet<Entry> Entries { get; set; }
     }
 }
---
+++ IntegrationApi/Controllers/EntriesController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatabaseConnection;
+using Models;
+
+namespace IntegrationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntriesController : ControllerBase
+    {
+        private readonly EntryContext _context;
+
+        public EntriesController(EntryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Entries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Entry>>> GetEntries()
+        {
+            return await _context.Entries
+                .Include(e => e.OfferDetails)
+                .ThenInclude(od => od.SellerContact)
+                .Include(e => e.PropertyAddress)
+                .Include(e => e.PropertyDetails)
+                .Include(e => e.PropertyPrice)
+                .Include(e => e.PropertyFeatures).ToListAsync();
+        }
+
+        // GET: api/Entries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Entry>> GetEntry(int id)
+        {
+            var entry = await _context.Entries
+                .Include(e => e.OfferDetails)
+                .ThenInclude(od => od.SellerContact)
+                .Include(e => e.PropertyAddress)
+                .Include(e => e.PropertyDetails)
+                .Include(e => e.PropertyPrice)
+                .Include(e => e.PropertyFeatures).FirstAsync(e => e.Id.Equals(id));
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return entry;
+        }
+
+        // PUT: api/Entries/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEntry(int id, Entry entry)
+        {
+            if (id != entry.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(entry).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EntryExists(id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Entries
+        [HttpPost]
+        public async Task<ActionResult<Entry>> PostEntry(Entry entry)
+        {
+            _context.Entries.Add(entry);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEntry", new { id = entry.Id }, entry);
+        }
+
+        // DELETE: api/Entries/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Entry>> DeleteEntry(int id)
+        {
+            var entry = await _context.Entries.FindAsync(id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            _context.Entries.Remove(entry);
+            await _context.SaveChangesAsync();
+
+            return entry;
+        }
+
+        private bool EntryExists(int id)
+        {
+            return _context.Entries.Any(e => e.Id == id);
+        }
+    }
+}
